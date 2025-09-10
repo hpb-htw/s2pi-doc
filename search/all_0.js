44cdaf323af89a59376ef6d8ff7107f2e64be13a
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2025-09-10 10:47:22 +0200

    auto commit

--- search/all_0.js
+++ search/all_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['asymptote_20werkzeuge_20zum_20zeichen_20von_20darstellenden_20geometrie_0',['Asymptote Werkzeuge zum Zeichen von Darstellenden Geometrie',['../md_README.html',1,'']]],
+  ['asymptote_20werkzeuge_20zum_20zeichen_20von_20darstellenden_20geometrie_0',['Asymptote Werkzeuge zum Zeichen von Darstellenden Geometrie',['../md_s2pi_README.html',1,'']]],
   ['aufrissprojekt_1',['aufrissProjekt',['../tools_8asy.html#abf157e4f2cd7f95f23630c9ce1694545',1,'tools.asy']]]
 ];
